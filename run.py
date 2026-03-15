6085b290b1cdabef4f37a208a07bf81458a58654
shtoppel <shtoppelkirill@gmail.com>
2026-03-15 22:01:33 +0100

    Bind dev servers to 0.0.0.0 to avoid empty localhost response

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if ROOT not in sys.path:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
